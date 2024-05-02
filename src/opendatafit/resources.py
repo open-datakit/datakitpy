8079d377d8e15c8e0f594dabb4d87598580aef8e
echus <varvara@echus.co>
2024-05-02 15:28:21 +1000

    Remove debugging

--- src/opendatafit/resources.py
+++ src/opendatafit/resources.py
@@ -11,9 +11,6 @@ class TabularDataResource:
     def __init__(self, resource: dict) -> None:
         """Load tabular data resource from JSON dict"""
 
-        print("resource:", resource["name"])
-        print("schema:", resource["schema"])
-
         # Load data into pandas DataFrame
         data = resource.pop("data")
         self._data = pd.DataFrame.from_dict(data)
