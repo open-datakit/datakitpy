fbe84873efb07adc4b57cdcd4c9ffb9b66e895b3
echus <varvara@echus.co>
2024-08-20 13:18:18 +1000

    Rename RESOURCES -> RESOURCES_PATH for clarity

--- src/opendatafit/datapackage.py
+++ src/opendatafit/datapackage.py
@@ -12,9 +12,9 @@ from .resources import TabularDataResource
 
 
 DEFAULT_BASE_PATH = os.getcwd()  # Default base datapackage path
-RESOURCES = "resources"
-METASCHEMAS = "metaschemas"
-ARGUMENTS = "arguments"
+RESOURCES_PATH = "resources"
+METASCHEMAS_PATH = "metaschemas"
+ARGUMENTS_PATH = "arguments"
 
 
 def load_argument_space(
@@ -24,7 +24,10 @@ def load_argument_space(
 ) -> dict:
     """Load a specified argument space"""
     with open(
-        f"{base_path}/{ARGUMENTS}/{algorithm_name}.{argument_space_name}.json",
+        (
+            f"{base_path}/{ARGUMENTS_PATH}/{algorithm_name}."
+            f"{argument_space_name}.json"
+        ),
         "r",
     ) as f:
         return json.load(f)
@@ -36,7 +39,7 @@ def write_argument_space(
 ) -> dict:
     """Write an argument space"""
     with open(
-        f"{base_path}/{ARGUMENTS}/{argument_space['name']}.json",
+        f"{base_path}/{ARGUMENTS_PATH}/{argument_space['name']}.json",
         "w",
     ) as f:
         json.dump(argument_space, f, indent=2)
@@ -49,7 +52,7 @@ def load_resource(
 ) -> TabularDataResource | dict:
     """Load a resource with the specified metaschema"""
     # Load resource with metaschema
-    resource_path = f"{base_path}/{RESOURCES}/{resource_name}.json"
+    resource_path = f"{base_path}/{RESOURCES_PATH}/{resource_name}.json"
 
     resource = None
 
@@ -59,7 +62,7 @@ def load_resource(
 
         # Load metaschema into resource object
         with open(
-            f"{base_path}/{METASCHEMAS}/{metaschema_name}.json", "r"
+            f"{base_path}/{METASCHEMAS_PATH}/{metaschema_name}.json", "r"
         ) as metaschema_file:
             resource_json["metaschema"] = json.load(metaschema_file)["schema"]
 
@@ -124,7 +127,9 @@ def write_resource(
     else:
         resource_json = resource
 
-    resource_path = f"{base_path}/{RESOURCES}/{resource_json['name']}.json"
+    resource_path = (
+        f"{base_path}/{RESOURCES_PATH}/{resource_json['name']}.json"
+    )
 
     # Remove metaschema before writing
     # This should have been loaded by load_argument
