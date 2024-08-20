1b9898f2f9facf1c166c4522a71b6cb75266f627
echus <varvara@echus.co>
2024-08-20 11:52:57 +1000

    Return parameter resources as TabularDataResource objects instead of dicts

--- src/opendatafit/datapackage.py
+++ src/opendatafit/datapackage.py
@@ -102,11 +102,11 @@ def load_resource(
 
             #     resource["schema"] = schema
 
-        if resource_json["profile"] == "tabular-data-resource":
+        if resource_json["profile"] == (
+            "tabular-data-resource" or "parameter-tabular-data-resource"
+        ):
+            # TODO: Create ParameterResource object to handle parameters
             resource = TabularDataResource(resource=resource_json)
-        elif resource_json["profile"] == "parameter-tabular-data-resource":
-            # TODO: Create ParameterResource object to handle this case
-            resource = resource_json
         else:
             raise NotImplementedError(
                 f"Unknown resource profile \"{resource_json['profile']}\""
